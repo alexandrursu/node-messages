0fa30ee74ad8420bfd79be8df3b81c6de2c38da5
Rene-Jacques Riel <r@r87.ca>
2016-01-24 19:39:42 -0500

    adding description to api and ui scripts

--- setup_api.sh
+++ setup_api.sh
@@ -1,3 +1,16 @@
+if [ $# -lt 1 ]
+  then
+    echo ""
+    echo ""
+    echo "This script can only be executed as such:"
+    echo "    ./setup_api.sh <mongo connection string>"
+    echo "For example:"
+    echo "    ./setup_api.sh mongodb://user:pwd@subdomain.mongolab.com:27017/messages"
+    echo ""
+    echo ""
+    exit 0
+fi
+
 echo $1 > api/mongo.config
 cd api
 node server.js &
--- setup_ui.sh
+++ setup_ui.sh
@@ -1,2 +1,17 @@
+if [ $# -lt 1 ]
+  then
+    echo ""
+    echo ""
+    echo "This script can only be executed as such:"
+    echo "    ./setup_ui.sh <messages api endpoint>"
+    echo "For example:"
+    echo "    ./setup_ui.sh http://localhost:8080/messages/"
+    echo ""
+    echo "Please note that the messages api endpoint must end with a forward slash '/'"
+    echo ""
+    echo ""
+    exit 0
+fi
+
 echo $1 > ui/endpoint.config
 http-server -p 8090 ./ui > /dev/null &
