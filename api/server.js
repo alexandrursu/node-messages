027834b93e4c7305a5bd5a6a1d483dae1ea5f261
Rene-Jacques Riel <r@r87.ca>
2016-01-27 00:56:52 -0500

    added API documentation

--- api/server.js
+++ api/server.js
@@ -28,6 +28,23 @@ router.route("/")
   });
 
 router.route("/messages")
+  /**
+   * @api {post} /messages/ Add Message
+   * @apiName AddMessage
+   * @apiGroup Message
+   * @apiVersion 1.0.0
+   *
+   * @apiDescription Add a message to the system
+   * @apiParam {String} text The text for the message
+   *
+   * @apiSuccess {ObjectID} id The unique identifier for the message
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "id": "56a5652c55ab891352f11fd0"
+   *     }
+   * @apiError (400) BadRequest The text parameter was not specified
+   */
   .post(function(req,res) {
     if (req.body.text == undefined || req.body.text == null) {
       res.status(400).json({ message: "post missing 'text' parameter" });
@@ -42,6 +59,29 @@ router.route("/messages")
         res.json({ id: message._id });
     });
   })
+  /**
+   * @api {get} /messages/ List Messages
+   * @apiName ListMessages
+   * @apiGroup Message
+   * @apiVersion 1.0.0
+   *
+   * @apiDescription List all messages in the system
+   * @apiSuccess {Object[]} messages A list of messages
+   * @apiSuccess {ObjectID} messages._id The unique identifier for the message
+   * @apiSuccess {String} messages.text The message text
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     [
+   *       {
+   *         "_id": "56a5652c55ab891352f11fd0",
+   *         "text": "first message here"
+   *       },
+   *       {
+   *         "_id": "56a5652c55ab891352f11fd5",
+   *         "text": "was it tims mitt i saw"
+   *       }
+   *     ]
+   */
   .get(function(req,res) {
     Message.find(function(err, messages) {
       if (err)
@@ -52,6 +92,22 @@ router.route("/messages")
   });
 
 router.route("/messages/:id")
+  /**
+   * @api {delete} /messages/:id Delete Message
+   * @apiName DeleteMessage
+   * @apiGroup Message
+   * @apiVersion 1.0.0
+   *
+   * @apiDescription Delete a message from the system
+   * @apiParam {ObjectID} :id The unique identifier for the message
+   * 
+   * @apiSuccess {String} message The message "record deleted" 
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "message": "record deleted"
+   *     }
+   */
   .delete(function(req,res) {
     Message.remove({
       _id:req.params.id
@@ -62,6 +118,29 @@ router.route("/messages/:id")
           res.status(200).json({ message: "record deleted"});
       });
   })
+  /**
+   * @api {get} /messages/:id Get Message Details
+   * @apiName GetMessage
+   * @apiGroup Message
+   * @apiVersion 1.0.0
+   *
+   * @apiDescription Give details about a message in the system
+   * @apiParam {ObjectID} :id The unique identifier for the message
+   * 
+   * @apiSuccess {Object} message The requested message 
+   * @apiSuccess {ObjectID} message._id The unique identifier for the message
+   * @apiSuccess {String} message.text The message text
+   * @apiSuccess {Boolean} message.is_palindrome If the message is a palindrome
+   * @apiSuccessExample {json} Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "_id": "56a5652c55ab891352f11fd0",
+   *       "text": "first message here"
+   *       "is_palindrome": false
+   *     }
+   * @apiError (404) NotFound The requested message was not found
+   * @apiError (500) InternalServerError The identifier specified was invalid
+   */
   .get(function(req,res) {
     Message.findById(req.params.id,function(err,message) {
       if (err)
